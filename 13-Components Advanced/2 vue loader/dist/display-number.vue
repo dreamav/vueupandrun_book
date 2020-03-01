be03813f58f8a94296559b45a147ade6961e5f28
Mevericks <reloved@gmail.com>
2020-03-01 22:29:33 +0200

    [13-Components Advanced] 03 настройка vue-loader и первый vue файл

---
+++ 13-Components Advanced/2 vue loader/dist/display-number.vue
@@ -0,0 +1,14 @@
+<template :number="number">
+    <p>The number is {{ number }}</p>
+</template>
+
+<script>
+    export default {
+        props: {
+            number: {
+                type: Number,
+                required: true
+            }
+        }
+    };
+</script>
---
+++ 13-Components Advanced/2 vue loader/dist/script.js
@@ -0,0 +1,8 @@
+import DisplayNumber from './display-number.vue';
+
+new Vue({
+    el: '#app',
+    components: {
+        DisplayNumber
+    }
+});
